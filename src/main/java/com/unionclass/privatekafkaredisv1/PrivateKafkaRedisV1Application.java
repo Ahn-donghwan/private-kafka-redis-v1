03433629d865af188e061fdeefea7707e1013a34
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-05-15 21:43:47 +0900

    chores: default settings

---
+++ src/main/java/com/unionclass/privatekafkaredisv1/PrivateKafkaRedisV1Application.java
@@ -0,0 +1,13 @@
+package com.unionclass.privatekafkaredisv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateKafkaRedisV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateKafkaRedisV1Application.class, args);
+    }
+
+}
